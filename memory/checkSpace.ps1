49fdb4612d5bdbf012003f231aff2609b1a33714
Aventhor <i@ksuharev.ru>
2025-07-07 18:50:55 +0300

    add: No close window step

--- memory/checkSpace.ps1
+++ memory/checkSpace.ps1
@@ -1,9 +1,16 @@
+#Checking space in userDirection
+#Gets infogrmation form C:\Users\userName\Downloads
+$downloads = (Get-ChildItem C:\Users\$env:USERNAME\Downloads -force -Recurse -ErrorAction SilentlyContinue| measure Length -sum).sum / 1Gb
+$documents = (Get-ChildItem C:\Users\$env:USERNAME\Documents -force -Recurse -ErrorAction SilentlyContinue| measure Length -sum).sum / 1Gb
+$desktop = (Get-ChildItem C:\Users\$env:USERNAME\Desktop -force -Recurse -ErrorAction SilentlyContinue| measure Length -sum).sum / 1Gb
 
-$currentUser = [Environment]::UserName
-Write-Host $currentUser
+# Outputs in console information 
+Write-Host ("Загрузки: ", $downloads) 
+Write-Host ("Документы: ", $documents) 
+Write-Host ("Рабочий стол: ", $desktop) 
 
 
-$downloads = (Get-ChildItem C:\Users\$currentUser\Downloads -force -Recurse -ErrorAction SilentlyContinue| measure Length -sum).sum / 1Gb
+# Do not lets close the window 
+Read-Host
 
-Write-Host ("Размер -- 'Загрузки': ", $downloads) 
 
